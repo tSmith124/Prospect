5783d0829d826fa3520cd22e971a34efbfaa2573
Taylor Smith <tdfrost004@gmail.com>
2024-01-11 17:11:43 -0800

    added proxy network

--- jobs.js
+++ jobs.js
@@ -1,10 +1,23 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const puppeteer = require('puppeteer')
+const proxyChain = require('proxy-chain')
 
 const INDEED_HOMEPAGE = ''
 const TEST_HOME = 'https://pptr.dev/'
 
 async function run() {
-  const browser = await puppeteer.launch({ headless: true })
+  const oldProxyUrl = `http://${process.env.PROXY_USER}:${process.env.PROXY_PASS}_country-UnitedStates@24.199.75.16:31112`
+
+  const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl)
+  console.log(newProxyUrl)
+
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [`--proxy-server=${newProxyUrl}`],
+  })
   const page = await browser.newPage()
   await page.goto(TEST_HOME)
   await page.screenshot({ path: 'example.png', fullPage: true })
